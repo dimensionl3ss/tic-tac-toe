5b731f322d85626ed33375ebe79f8e7b0a02df2c
Adarsh <adarshraj0210@gmail.com>
2021-08-03 12:39:56 +0530

    error fix and  style components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './style/root.scss'
 import { Board } from './components/Board';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>TIC TAC TOE!</h1>
       <Board/>
     </div>
